f4a3246daa2571fa5be3bc6bc00f9b051c2d39ef
Francisco Alfredo Uribe Sánchez <alfredo.uribe.sanchez@gmail.com>
2022-04-03 20:08:10 -0500

    add: android build

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -21,10 +21,14 @@ export class DataService {
 
     const options = {
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Accept': 'application/json, text/plain'
       }
     }
 
-    return this.http.post<any>(`http://localhost:8000/api/process_command/`, JSON.stringify(postData), options)
+    return this.http.post<any>(`https://staging.alfredouribe.com/api/process_command/`, JSON.stringify(postData), options)
+    //return this.http.post<any>(`http://192.168.100.33:8000/api/process_command/`, JSON.stringify(postData), options)
   }
 }
