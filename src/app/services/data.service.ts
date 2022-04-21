882cc2b51a6da0e8f1f301b1ae8091e09944b0aa
Francisco Alfredo Uribe Sánchez <alfredo.uribe.sanchez@gmail.com>
2022-04-20 19:44:20 -0500

    add: change ip of api service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -28,7 +28,10 @@ export class DataService {
       }
     }
 
-    return this.http.post<any>(`https://staging.alfredouribe.com/api/process_command/`, JSON.stringify(postData), options)
+    console.log(postData)
+    console.log(options)
+    return this.http.post<any>(`http://18.215.30.22/api/process_command`, postData, options)
+    //return this.http.post<any>(`http://staging.alfredouribe.com/api/process_command`, postData, options)
     //return this.http.post<any>(`http://192.168.100.33:8000/api/process_command/`, JSON.stringify(postData), options)
   }
 }
